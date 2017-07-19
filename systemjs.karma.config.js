9eb391426dac80e80fb9b2338b9a6af0cc3585cc
rfprod <zoidenmacht@zoho.com>
2017-07-19 07:16:57 -0700

    (fix) systemjs config for karma tests

--- systemjs.karma.config.js
+++ systemjs.karma.config.js
@@ -12,36 +12,24 @@
 		'ng2-nvd3': 														'npm:ng2-nvd3/build/lib',
 		'rxjs': 																'npm:rxjs',
 		'traceur': 															'npm:traceur/bin',
-		'@angular/animations': 									'npm:@angular/animations/bundles',
-		'@angular/animations/browser': 					'npm:@angular/animations/bundles',
-		'@angular/core': 												'npm:@angular/core/bundles',
-		'@angular/common': 											'npm:@angular/common/bundles',
-		'@angular/compiler': 										'npm:@angular/compiler/bundles',
-		'@angular/forms': 											'npm:@angular/forms/bundles',
-		'@angular/http': 												'npm:@angular/http/bundles',
-		'@angular/platform-browser': 						'npm:@angular/platform-browser/bundles',
-		'@angular/platform-browser/animations':	'npm:@angular/platform-browser/bundles',
-		'@angular/platform-browser-dynamic': 		'npm:@angular/platform-browser-dynamic/bundles',
-		'@angular/router': 											'npm:@angular/router/bundles'
+		'@angular/animations': 									'npm:@angular/animations/bundles/animations.umd.js',
+		'@angular/animations/browser': 					'npm:@angular/animations/bundles/animations-browser.umd.js',
+		'@angular/core': 												'npm:@angular/core/bundles/core.umd.js',
+		'@angular/common': 											'npm:@angular/common/bundles/common.umd.js',
+		'@angular/compiler': 										'npm:@angular/compiler/bundles/compiler.umd.js',
+		'@angular/forms': 											'npm:@angular/forms/bundles/forms.umd.js',
+		'@angular/http': 												'npm:@angular/http/bundles/http.umd.js',
+		'@angular/platform-browser': 						'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+		'@angular/platform-browser/animations':	'npm:@angular/platform-browser/bundles/platform-browser-animations.umd.js',
+		'@angular/platform-browser-dynamic': 		'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+		'@angular/router': 											'npm:@angular/router/bundles/router.umd.js'
 	};
 	// how to load packages
 	var packages = {
 		'app': 												{ main: 'app', defaultExtension: 'js' },
 		'ng2-nvd3': 									{ main: 'ng2-nvd3', defaultExtension: 'js' },
 		'rxjs': 											{ main: 'Rx', defaultExtension: 'js' },
-		'traceur': 										{ main: 'traceur', defaultExtension: 'js' },
-
-		'@angular/animations': 									{ main: 'animations', defaultExtension: 'umd.js' },
-		'@angular/animations/browser': 					{ main: 'animations-browser', defaultExtension: 'umd.js' },
-		'@angular/core': 												{ main: 'core', defaultExtension: 'umd.js' },
-		'@angular/common': 											{ main: 'common', defaultExtension: 'umd.js' },
-		'@angular/compiler': 										{ main: 'compiler', defaultExtension: 'umd.js' },
-		'@angular/forms': 											{ main: 'forms', defaultExtension: 'umd.js' },
-		'@angular/http': 												{ main: 'http', defaultExtension: 'umd.js' },
-		'@angular/platform-browser': 						{ main: 'platform-browser', defaultExtension: 'umd.js' },
-		// '@angular/platform-browser/animations': { main: 'platform-browser-animations', defaultExtension: 'umd.js' },
-		'@angular/platform-browser-dynamic': 		{ main: 'platform-browser-dynamic', defaultExtension: 'umd.js' },
-		'@angular/router': 											{ main: 'router', defaultExtension: 'umd.js' }
+		'traceur': 										{ main: 'traceur', defaultExtension: 'js' }
 	};
 
 	var config = {
