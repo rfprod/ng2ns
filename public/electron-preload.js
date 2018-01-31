e52a368569cc3b43c94d1fb74d8cb9989eeb385c
rfprod <zoidenmacht@zoho.com>
2018-01-31 13:57:18 -0800

    (update) electron preload - shoft+ctrl+i toggles dev tools

--- public/electron-preload.js
+++ public/electron-preload.js
@@ -9,4 +9,15 @@ const _clearImmediate = clearImmediate;
 process.once('loaded', () => {
 	global.setImmediate = _setImmediate;
 	global.clearImmediate = _clearImmediate;
+
+	/*
+	*	dev tools toggle hotkey: SHIFT+CTRL+i
+	*/
+	document.addEventListener('keydown', (keydownEvent) => {
+		const eventObj = window.event ? event : keydownEvent;
+		keydownEvent.preventDefault();
+		if (eventObj.shiftKey && eventObj.ctrlKey && eventObj.keyCode === 73) { // Shift + Ctrl + i
+			require('remote').getCurrentWindow().toggleDevTools();
+		}
+	});
 });
